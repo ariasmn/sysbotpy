bf1640c20276cbbd19d1f9616ae3101ec56d49ba
Ismael Arias <ismaelariasmn@gmail.com>
2019-05-25 16:46:37 +0200

    Implemented InlineKeyboard. It allows you to shutdown and restart each host. I might not be using correct functions, but the API doc and snippets are either insufficient or I don´t understand it.

--- server/test.py
+++ server/test.py
@@ -1,11 +1,22 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
 import re
 import socket
 import sys
 import json
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 PORT = 1337
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def is_up(addr):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,7 +41,7 @@ def get_data(hosts):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error:
-            sys.exit()
+            print (socket.error)
         s.connect((host , PORT))
         request = ("info").encode()
         try:
@@ -42,17 +53,62 @@ def get_data(hosts):
         hosts_json.append(reply)
     return hosts_json
 
-def bop(bot, update):
+def shutdown_host (host):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print (socket.error)
+    s.connect((host, PORT))
+    request = ("shutdown").encode()
+    try:
+        s.sendall(request)
+    except socket.error:
+        print ('Send failed')
+        sys.exit()
+
+def all(bot, update):
     data = get_data(run('192.168.1.'))
     for each_host in data:
         each_host = json.loads(each_host)
+
+        keyboard = [[InlineKeyboardButton("Apagar", callback_data=each_host['ip_addr'] + ",shutdown"),
+                    InlineKeyboardButton("Reiniciar", callback_data=each_host['ip_addr'] + ",restart")]]
+
+        
         chat_id = update.message.chat_id
-        bot.send_message(chat_id=chat_id, text=each_host['os'])
+        message = ("- *Sistema operativo* : " + each_host['os'] + 
+            "\n- *Nombre de host* : " + each_host['hostname'] + 
+            "\n- *Usuario logeado* : " + each_host['user_logged'] +
+            "\n- *IP local* : " + each_host['ip_addr'])
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        bot.send_message(chat_id=chat_id, text=message, parse_mode="Markdown", reply_markup = reply_markup)
+
+def inline_button_callback(bot, update):
+    query = update.callback_query
+    """ bot.send_message(text="Selected option: {}".format(query.data),
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id) """
+    
+    what_to_do = query.data.split(',')[1]
+    host_ip = query.data.split(',')[0]
+    if what_to_do == "shutdown":
+        shutdown_host(host_ip)
+    elif what_to_do == "restart":
+        print ("restart")
+    else:
+        print ("something went wrong")
+    #print (query.data.split(',')[0])
+
 
 def main():
     updater = Updater('812262356:AAF1nhoDeCKaZzGax_wpFSuUsLD2c-1gGB0')
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',bop))
+
+    dp.add_handler(CommandHandler('all',all))
+    dp.add_handler(CallbackQueryHandler(inline_button_callback))
+    dp.add_error_handler(error)
+
     updater.start_polling()
     updater.idle()
 
